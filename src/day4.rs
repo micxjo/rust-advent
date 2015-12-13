20c9c8ad9642bf5b94bf152c249b604e042fe742
Micxjo Funkcio <micxjo@fastmail.com>
2015-12-13 02:32:32 -0800

    Make day 4 faster and remove rustc_serialize dep

--- src/day4.rs
+++ src/day4.rs
@@ -1,28 +1,27 @@
 extern crate libc;
-extern crate rustc_serialize;
 
 use self::libc::c_long;
-use self::rustc_serialize::hex::*;
 
 #[link(name="crypto")]
 extern "C" {
     fn MD5(input: *const u8, size: c_long, output: *mut u8) -> *mut u8;
 }
 
-fn md5(src: &str) -> String {
+fn md5(src: &str) -> Vec<u8> {
     unsafe {
         let mut digest: Vec<u8> = Vec::with_capacity(16);
         MD5(src.as_ptr(), src.len() as c_long, digest.as_mut_ptr());
         digest.set_len(16);
-        digest[..].to_hex()
+        digest
     }
 }
 
-pub fn find_hash(prefix: &str, search: &str) -> u64 {
+pub fn find_hash(prefix: &str, digits: u8) -> u64 {
+    let shift = if digits == 5 { 4 } else { 0 };
     for i in 1.. {
         let msg = format!("{}{}", prefix, i);
         let digest = md5(&msg);
-        if digest.starts_with(search) {
+        if digest[0] == 0 && digest[1] == 0 && (digest[2] >> shift) == 0 {
             return i;
         }
     }
@@ -35,8 +34,8 @@ pub fn process_file(path: &str) {
     let mut file = File::open(path).unwrap();
     let mut string = String::new();
     file.read_to_string(&mut string).unwrap();
-    println!("Part 1: {}", find_hash(&string, "00000"));
-    println!("Part 2: {}", find_hash(&string, "000000"));
+    println!("Part 1: {}", find_hash(&string, 5));
+    println!("Part 2: {}", find_hash(&string, 6));
 }
 
 #[cfg(test)]
@@ -45,7 +44,7 @@ mod tests {
 
     #[test]
     fn test_find_hash() {
-        assert_eq!(find_hash("abcdef", "00000"), 609043);
-        assert_eq!(find_hash("pqrstuv", "00000"), 1048970);
+        assert_eq!(find_hash("abcdef", 5), 609043);
+        assert_eq!(find_hash("pqrstuv", 5), 1048970);
     }
 }
