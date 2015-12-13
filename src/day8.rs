b6cf6bdc2ce414e9aa11556cab4f050318d3e1d8
Micxjo Funkcio <micxjo@fastmail.com>
2015-12-13 15:47:10 -0800

    Add day 8, part 2

--- src/day8.rs
+++ src/day8.rs
@@ -15,6 +15,33 @@ pub fn char_count(string: &str) -> usize {
     sum + 1
 }
 
+pub fn encoded_count(string: &str) -> usize {
+    let bytes = string.as_bytes();
+    let mut sum = 0;
+    let mut i = 0;
+    while i < bytes.len() - 1 {
+        match (bytes[i] as char, bytes[i + 1] as char) {
+            ('\\', '\"') => {
+                sum += 2;
+                i += 2;
+            }
+            ('\\', '\\') => {
+                sum += 2;
+                i += 2;
+            }
+            ('\\', 'x') => {
+                sum += 1;
+                i += 4;
+            }
+            _ => {
+                i += 1;
+            }
+        }
+    }
+
+    sum + 4
+}
+
 pub fn process_file(path: &str) {
     use std::fs::File;
     use std::io::prelude::*;
@@ -22,18 +49,20 @@ pub fn process_file(path: &str) {
 
     let file = File::open(path).unwrap();
     let file = BufReader::new(file);
-    let mut part1 = 0;
+    let (mut part1, mut part2) = (0, 0);
     for line in file.lines() {
         let line = line.unwrap();
         part1 += char_count(&line);
+        part2 += encoded_count(&line);
     }
     println!("Part 1: {}", part1);
+    println!("Part 2: {}", part2);
 }
 
 
 #[cfg(test)]
 mod tests {
-    use super::char_count;
+    use super::{char_count, encoded_count};
 
     #[test]
     fn test_char_count() {
@@ -41,5 +70,13 @@ mod tests {
         assert_eq!(char_count("\"abc\""), 2);
         assert_eq!(char_count("\"aaa\\\"aaa\""), 3);
         assert_eq!(char_count("\"\\x27\""), 5);
+    }
+
+    #[test]
+    fn test_encoded_count() {
+        assert_eq!(encoded_count("\"\""), 4);
+        assert_eq!(encoded_count("\"abc\""), 4);
+        assert_eq!(encoded_count("\"aaa\\\"aaa\""), 6);
+        assert_eq!(encoded_count("\"\\x27\""), 5);
     }
 }
