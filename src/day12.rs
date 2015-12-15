ec390ea947202ab76134107d8cf9a2bc26357446
Micxjo Funkcio <micxjo@fastmail.com>
2015-12-14 22:52:58 -0800

    Add day 12, part 2

--- src/day12.rs
+++ src/day12.rs
@@ -20,6 +20,33 @@ pub fn json_sum_str(json: &str) -> i64 {
     }
 }
 
+pub fn json_sum_unred(value: &Value) -> i64 {
+    match *value {
+        Value::I64(n) => n,
+        Value::U64(n) => n as i64,
+        Value::Array(ref vec) => {
+            vec.iter()
+               .map(json_sum_unred)
+               .fold(0, Add::add)
+        }
+        Value::Object(ref o) => {
+            if o.values().any(|v| v == &Value::String("red".to_owned())) {
+                0
+            } else {
+                o.values().map(json_sum_unred).fold(0, Add::add)
+            }
+        }
+        _ => 0,
+    }
+}
+
+pub fn json_sum_unred_str(json: &str) -> i64 {
+    match serde_json::from_str(json) {
+        Ok(val) => json_sum_unred(&val),
+        Err(_) => 0,
+    }
+}
+
 pub fn process_file(path: &str) {
     use std::fs::File;
     use std::io::prelude::*;
@@ -28,11 +55,12 @@ pub fn process_file(path: &str) {
     file.read_to_string(&mut string).unwrap();
     let json = serde_json::from_str(&string).unwrap();
     println!("Part 1: {}", json_sum(&json));
+    println!("Part 2: {}", json_sum_unred(&json));
 }
 
 #[cfg(test)]
 mod tests {
-    use super::json_sum_str;
+    use super::{json_sum_str, json_sum_unred_str};
 
     #[test]
     fn test_json_sum() {
@@ -45,5 +73,15 @@ mod tests {
         assert_eq!(json_sum_str("[]"), 0);
         assert_eq!(json_sum_str("[[]]"), 0);
         assert_eq!(json_sum_str("{}"), 0);
+    }
+
+    #[test]
+    fn test_json_sum_unred() {
+        assert_eq!(json_sum_unred_str("[1,2,3]"), 6);
+        assert_eq!(json_sum_unred_str("[1,{\"c\":\"red\",\"b\":2},3]"), 4);
+        assert_eq!(json_sum_unred_str("{\"d\":\"red\",\"e\":[1,2,3,4],\"f\":\
+                                       5}"),
+                   0);
+        assert_eq!(json_sum_unred_str("[1,\"red\",5]"), 6);
     }
 }
