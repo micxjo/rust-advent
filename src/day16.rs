d31efac11bddb6d3c30a4737526f1b2a50891cea
Micxjo Funkcio <micxjo@fastmail.com>
2015-12-17 20:14:58 -0800

    Add day 16, part 2

--- src/day16.rs
+++ src/day16.rs
@@ -1,10 +1,56 @@
 use std::collections::HashMap;
 
-pub fn process_file(path: &str) {
+fn check_thing(analysis: &HashMap<String, u32>,
+               thing: &str,
+               count: u32,
+               part: u32)
+               -> bool {
+    let real_count = analysis.get(thing).unwrap();
+    if part == 2 && (thing == "cats:" || thing == "trees:") {
+        &count > real_count
+    } else if part == 2 && (thing == "pomeranians:" || thing == "goldfish:") {
+        &count < real_count
+    } else {
+        &count == real_count
+    }
+}
+
+fn process_part(analysis: &HashMap<String, u32>, path: &str, part: u32) {
     use std::fs::File;
     use std::io::prelude::*;
     use std::io::BufReader;
 
+    let file = File::open(path).unwrap();
+    let file = BufReader::new(file);
+    for line in file.lines() {
+        let line = line.unwrap();
+        let words: Vec<_> = line.split(" ").collect();
+
+        let thing = words[2].to_owned();
+        let count = words[3].trim_matches(',').parse::<u32>().unwrap();
+        if !check_thing(&analysis, &thing, count, part) {
+            continue;
+        }
+
+        let thing = words[4].to_owned();
+        let count = words[5].trim_matches(',').parse::<u32>().unwrap();
+        if !check_thing(&analysis, &thing, count, part) {
+            continue;
+        }
+
+        let thing = words[6].to_owned();
+        let count = words[7].trim_matches(',').parse::<u32>().unwrap();
+        if !check_thing(&analysis, &thing, count, part) {
+            continue;
+        }
+
+        println!("Part {}: {}", part, words[1].trim_matches(':').to_owned());
+        break;
+    }
+
+}
+
+pub fn process_file(path: &str) {
     let mut analysis: HashMap<String, u32> = HashMap::new();
     analysis.insert("children:".to_owned(), 3);
     analysis.insert("cats:".to_owned(), 7);
@@ -17,35 +63,6 @@ pub fn process_file(path: &str) {
     analysis.insert("cars:".to_owned(), 2);
     analysis.insert("perfumes:".to_owned(), 1);
 
-    let file = File::open(path).unwrap();
-    let file = BufReader::new(file);
-    for line in file.lines() {
-        let line = line.unwrap();
-        let words: Vec<_> = line.split(" ").collect();
-
-        let thing = words[2].to_owned();
-        let count = words[3].trim_matches(',').parse::<u32>().unwrap();
-        match analysis.get(&thing) {
-            Some(n) if n == &count => {}
-            _ => continue,
-        }
-
-        let thing = words[4].to_owned();
-        let count = words[5].trim_matches(',').parse::<u32>().unwrap();
-        match analysis.get(&thing) {
-            Some(n) if n == &count => {}
-            _ => continue,
-        }
-
-        let thing = words[6].to_owned();
-        let count = words[7].trim_matches(',').parse::<u32>().unwrap();
-        match analysis.get(&thing) {
-            Some(n) if n == &count => {}
-            _ => continue,
-        }
-
-        println!("Part 1: {}", words[1].trim_matches(':').to_owned());
-        break;
-    }
-
+    process_part(&analysis, path, 1);
+    process_part(&analysis, path, 2);
 }
